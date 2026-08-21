11750dacc4575800b1da2eb3c3ab26ea31af0e7a
Nika Svencbirž <svencbir.nika@gmail.com>
2026-08-21 17:59:27 +0200

    Popravila sem, da mi je leto pravilno prebralo, ker prej ni vrnilo nobenega podatka.

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -11,7 +11,9 @@ def prenesi_spot_cene(leto=2025, drzava="SI"):
     os.makedirs(MAPA_PODATKI, exist_ok=True)
     pot_datoteke = os.path.join(MAPA_PODATKI, f"spot_cene_{drzava}_{leto}.csv")
 
-    url = f"https://api.energy-charts.info/price?country={drzava}"
+    start = f"{leto}-01-01"
+    end = f"{leto}-12-31"
+    url = f"https://api.energy-charts.info/price?bzn={drzava}&start={start}&end={end}"
     print(f"Prenašam SPOT cene za državo {drzava} za leto {leto}...")
     #Sporoči morebitno napako
     odziv = requests.get(url, timeout=30)
@@ -19,7 +21,7 @@ def prenesi_spot_cene(leto=2025, drzava="SI"):
         raise ConnectionError(f"Napaka pri prenosu SPOT cen: koda {odziv.status_code}")
 
     podatki = odziv.json()
-    casovni_zigi = pd.to_datatime(podatki["unix_seconds"], unit = "s")
+    casovni_zigi = pd.to_datetime(podatki["unix_seconds"], unit = "s")
     cene = podatki ["price"]
 
     df_spot = pd.DataFrame({
@@ -27,9 +29,9 @@ def prenesi_spot_cene(leto=2025, drzava="SI"):
         "SPOT_EUR_MWh": cene
     })
 
-    #Filtrira za izbrano leto
-    df_spot = df_spot[df_spot["Timestamp"].dt.year == leto].copy()
     #Shranim v CSV
     df_spot.to_csv(pot_datoteke, index=False)
     print(f" -> SPOT cene uspešno shranjene v: {pot_datoteke} (št. vrstic: {len(df_spot)})")
     return df_spot
+if __name__ == "__main__":
+    prenesi_spot_cene(leto=2025, drzava="SI")
