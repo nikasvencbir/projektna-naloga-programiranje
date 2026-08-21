96151d566cc8a578660ddd2658745da44c6dd9cc
Nika Svencbirž <svencbir.nika@gmail.com>
2026-08-21 17:38:50 +0200

    Dodala sem da v sporoči napako.

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -1,5 +1,7 @@
 """Modul za prenos podatkov o borznih cenah elektrike in sončnem obsevanju."""
 import os
+import requests
+import json
 
 #Določim pot do mape "podatki", ki sem jo ustvarila prej, sem se bojo shranjevali podatki iz spleta.
 MAPA_PODATKI = os.path.join(os.path.dirname(__file__),"podatki")
@@ -11,4 +13,8 @@ def prenesi_spot_cene(leto=2025, drzava="SI"):
     url = f"https://api.energy-charts.info/price?country={drzava}"
     print(f"Prenašam SPOT cene za državo {drzava} za leto {leto}...")
     #Sporoči morebitno napako
-    
+    odziv = requests.get(url, timeout=30)
+    if odziv.status_code != 200:
+        raise ConnectionError(f"Napaka pri prenosu SPOT cen: koda {odziv.status_code}")
+
+    podatki = odziv.json()
